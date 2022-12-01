2eaaa838ac645a83933832a4c0745358787db022
houressam <houaria.ressam@stonal.com>
2022-12-01 15:10:28 +0100

    test should add position

--- src/test/kotlin/com/formation/marsrover/domain/PositionTest.kt
+++ src/test/kotlin/com/formation/marsrover/domain/PositionTest.kt
@@ -1,9 +1,20 @@
 package com.formation.marsrover.domain
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
 internal class PositionTest {
 
-    //TODO : test plus operator
+    @Test
+    fun `should add position`() {
+        //Given
+        val position1 = Position(x = 0, y = 0)
+        val position2 = Position(x = 1, y = 1)
 
+        //when
+        val expectedPosition = position1.plus(position2)
+
+        //Then
+        assertThat(expectedPosition).isEqualTo(Position(x = 1, y = 1))
+    }
 }
